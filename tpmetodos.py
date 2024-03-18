e90f4cbb019ae4fa5387ea19f6d07c1049608c87
Anaholcman <anitaholcman@gmail.com>
2024-03-18 09:55:45 -0300

    ni idea

--- tpmetodos.py
+++ tpmetodos.py
@@ -1,2 +1,15 @@
-print("hola tizzzzzz")
-print("huola anu")
+## ej1
+##habria que hacer interpolacion lineal para acercarnos a todos los puntos
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+##with open mnyo_mediciones as mediciones
+
+def separarNumeros():
+    x = []
+    y = []
+    for i in range(len(mediciones)):
+        x.append(mediciones[i][0])
+        y.append(mediciones[i][1])
+    return x, y
