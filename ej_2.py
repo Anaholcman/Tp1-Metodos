3826210a3b6abef600f65097e980d19779ddf27b
Anaholcman <anitaholcman@gmail.com>
2024-03-20 12:56:26 -0300

    ej2 comenzado

--- ej_2.py
+++ ej_2.py
@@ -1,42 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import lagrange
 
-# Leer los datos del archivo CSV
-data_graf1 = np.loadtxt('mnyo_tp01_2024_1s/mnyo_mediciones.csv')
+data = np.loadtxt('mnyo_tp01_2024_1s/mnyo_mediciones.csv')
+x = data[:, 0]
+y = data[:, 1]
 
-# Extraer las columnas x e y del primer archivo
-x_1 = data_graf1[:, 0]
-y_1 = data_graf1[:, 1]
+puntos_4 = np.loadtxt("mnyo_tp01_2024_1s/mnyo_mediciones2.csv")
+x_4 = puntos_4[:, 0]
+y_4 = puntos_4[:, 1]
 
-data_ground_truth = np.loadtxt('mnyo_ground_truth.csv')
+poly = lagrange(x_4, y_4)
 
-# Extraer las columnas x e y del segundo archivo
-x_t = data_ground_truth[:, 0]
-y_t = data_ground_truth[:, 1]
+print(poly)
 
-
-
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-
-##graf 1
-ax1.scatter(x_1, y_1, color='blue', label='Puntos')
-ax1.plot(x_1, y_1, color='red', linestyle='-', marker='', label='Líneas')
-ax1.set_xlabel('X')
-ax1.set_ylabel('Y')
-ax1.set_title('Gráfico de puntos con líneas')
-ax1.legend()
-ax1.grid(True)
-
-##graf original
-ax2.scatter(x_t, y_t, color='blue', label='Puntos')
-ax2.plot(x_t, y_t, color='red', linestyle='-', marker='', label='Líneas')
-ax2.set_xlabel('X')
-ax2.set_ylabel('Y')
-ax2.set_title('Gráfico original')
-ax2.legend()
-ax2.grid(True)
+def y(x):
+    return poly(x)
 
 
-plt.tight_layout() 
+
+
+
+
+
+
+plt.figure(figsize=(8, 6))
+plt.scatter(x, y, color='blue', label='Puntos')
+plt.plot(x, y, color='red', linestyle='-', marker='', label='Recorrido 1era trayectoria')
+plt.plot(x_4, y_4, color='green', linestyle='-', marker='', label='Recorrido 2nda trayectoria')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.scatter(x_4, y_4, label='4 mediciones', color='orange')
+plt.title('Grafico de las 2 trayectorias')
+plt.legend()
+plt.grid(True)
 plt.show()
+
 
