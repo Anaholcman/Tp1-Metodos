521ee4086d0992ccdab0a168e17543359d17f97f
Anaholcman <anitaholcman@gmail.com>
2024-03-20 10:48:12 -0300

    int_lineal, chernobyl

---
+++ int_lineal.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import lagrange
+
+
+
+def f_original(x):
+    return (0.3) ** np.abs(x) * np.sin(4 * x) - np.tanh(2 * x) + 2
+
+def absolute_error(original_values, interpolated_values):
+    return np.abs(original_values - interpolated_values)
+
+def relative_error(original_values, interpolated_values):
+    return np.abs((original_values - interpolated_values) / original_values)
+
+
+def calculate_errors(original_values, interpolated_values):
+    abs_err = absolute_error(original_values, interpolated_values)
+    rel_err = relative_error(original_values, interpolated_values)
+    
+    max_abs_err = np.max(abs_err)
+    max_rel_err = np.max(rel_err)
+    
+    avg_abs_err = np.mean(abs_err)
+    avg_rel_err = np.mean(rel_err)
+    
+    return max_abs_err, max_rel_err, avg_abs_err, avg_rel_err
+
+
+
+##### Interpolación lineal de Lagrange #####
+x_data = np.linspace(-4, 4, num=15)  # Usamos 20 puntos equidistantes
+y_data = f_original(x_data)
+
+polys = []
+
+# Interpolar linealmente entre los puntos de datos
+for i in range(len(x_data) - 1):
+    poly = lagrange([x_data[i], x_data[i+1]], [y_data[i], y_data[i+1]])
+    polys.append(poly)
+
+# Crear un conjunto de puntos para graficar las interpolaciones lineales
+x_values = np.linspace(-4, 4, 1000)
+y_values_interpolated = np.zeros_like(x_values)
+
+# Evaluar cada polinomio de Lagrange en su intervalo correspondiente
+for i, poly in enumerate(polys):
+    mask = (x_values >= x_data[i]) & (x_values <= x_data[i+1])
+    y_values_interpolated[mask] = poly(x_values[mask])
+
+max_abs_err, max_rel_err, avg_abs_err, avg_rel_err = calculate_errors(f_original(x_values), y_values_interpolated)
+
+error_text = f"Error Absoluto Máximo: {max_abs_err:.5f}\n"\
+                f"Error Absoluto Promedio: {avg_abs_err:.5f}\n\n"\
+                f"Error Relativo Máximo: {max_rel_err:.5f}\n"\
+                f"Error Relativo Promedio: {avg_rel_err:.5f}"   
+
+
+plt.plot()
+    
+# Graficar la función original y las interpolaciones lineales
+plt.figure(figsize=(10, 6))
+plt.plot(x_values, f_original(x_values), label='Función Original', color='blue')
+plt.plot(x_values, y_values_interpolated, label='Interpolación Lineal de Lagrange', linestyle='--', color='red')
+plt.scatter(x_data, y_data, label='Puntos de Datos', color='black')
+plt.title('Interpolación Lineal de Lagrange')
+plt.text(-4, np.min(f_original(x_values)), error_text, fontsize=8, va='bottom', ha='left', color='red',  bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.grid(True)
+plt.show()
---
+++ tizierror.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import lagrange
+
+def f_a(x):
+    return (0.3**(abs(x))) * np.sin(4*x) - np.tanh(2*x) + 2
+
+def f_b(x1, x2):
+    return 0
+
+def equispaced_dataset_build(start, stop, points_number, f): # points_number>=2
+    data_set = []
+    for x_i in np.linspace(start, stop, points_number):     # genera puntos equiespaciados
+        data_set.append([x_i, f(x_i)])
+    return data_set
+
+def lagrange_interpolation(data_set):
+    x_values = [point[0] for point in data_set]
+    y_values = [point[1] for point in data_set]
+    interpolator = lagrange(x_values, y_values)     
+    return interpolator                             
+
+def lagrange_iterator(start, stop, f, degree):
+    degree_evolution = {}
+    for i in range(2,degree+2):                                 # quiero generar hasta n+1 puntos, que me dará el mayor grado que quiero (n)
+        data_set = equispaced_dataset_build(start, stop, i, f)
+        degree_evolution[i-1]=lagrange_interpolation(data_set)
+    return degree_evolution
+
+def error_median(interpolator, f, x_values):
+    y_values_interp = interpolator(x_values)
+    y_values_exact = f(x_values)
+    error = np.abs(y_values_exact - y_values_interp)
+    print(error.max())
+    return np.median(error)
+
+def main():
+    start = -4
+    stop = 4
+    degree = 30
+    x_values = np.linspace(start, stop, 1000)
+    errors = []
+    interpolators_dict = lagrange_iterator(start, stop, f_a, degree)
+    
+    for i in range(2, degree+2):
+        interpolator = interpolators_dict[i-1]
+        error = error_median(interpolator, f_a, x_values)
+        errors.append(error)
+    
+    plt.plot(range(1, degree + 1), errors, marker='o')
+    plt.xlabel('Grado del polinomio interpolante')
+    plt.ylabel('Error promedio de interpolación')
+    plt.title('Error de interpolación vs. Grado del polinomio')
+    plt.grid(True)
+    #plt.show()
+
+    # podemos evaluar cual es el grado del polinomio que se corresponde con el menor error, y comparar ambos gráficos
+        
+    # y_values = f_a(x_values)
+
+    # # Graficar la función
+    # plt.plot(x_values, y_values, label='f_a(x)')
+    # plt.xlabel('x')
+    # plt.ylabel('f_a(x)')
+    # plt.title('Gráfico de la función f_a(x)')
+    # plt.grid(True)
+    # plt.legend()
+    # plt.show()
+
+if __name__ == "__main__":
+    main()
+
+
