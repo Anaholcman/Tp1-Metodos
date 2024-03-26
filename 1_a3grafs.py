b052682bae7d61ec4304327b04129cd77cbdfb38
Anaholcman <anitaholcman@gmail.com>
2024-03-26 18:54:34 -0300

    1bgrafs bien

--- 1_a3grafs.py
+++ 1_a3grafs.py
@@ -13,7 +13,7 @@ def f_original(x):
 
 ##interpolacion lineal de lagrange----------------------
 
-x_data_lineal = np.linspace(-4, 4, num=8)  
+x_data_lineal = np.linspace(-4, 4, num=10)  
 y_data_lineal = f_original(x_data_lineal)
 
 polys = []
@@ -34,7 +34,7 @@ for i, poly in enumerate(polys):# Evalua cada polinomio de Lagrange en su interv
     
 #interpolacion polinomica----------------
 
-x_interp_lagrange = np.linspace(-4, 4, 8)
+x_interp_lagrange = np.linspace(-4, 4, 10)
 y_interp_lagrange = f_original(x_interp_lagrange)
 
 interpolator_poli = lagrange(x_interp_lagrange, y_interp_lagrange)
@@ -45,7 +45,7 @@ y_interp_points = interpolator_poli(x_values_polinomic)
 
 # Interpolación cúbica----------
 
-x_interp_splines = np.linspace(-4, 4, 8)  
+x_interp_splines = np.linspace(-4, 4, 10)  
 y_interp_splines = f_original(x_interp_splines)
 
 spline = CubicSpline(x_interp_splines, y_interp_splines)
@@ -104,7 +104,7 @@ error_text_spline = calculate_errors(f_original(x_value_splines), y_splines_inte
 error_text_polinomic = calculate_errors(f_original(x_values_polinomic), y_interp_points)
 
 
-fig, ax = plt.subplots(3, 1, figsize=(8, 12))
+fig, ax = plt.subplots(3, 1, figsize=(12, 15))
 
 # Interpolación lineal
 ax[0].plot(x_values_lineal, f_original(x_values_lineal), label='Función Original', color='blue')
@@ -122,7 +122,7 @@ ax[1].plot(x_values_polinomic, f_original(x_values_polinomic), label='Función O
 ax[1].plot(x_values_polinomic, y_interp_points, label='Interpolación', linestyle='--', color='red')
 ax[1].scatter(x_interp_lagrange, y_interp_lagrange, label='Puntos de Datos', color='black')
 ax[1].set_title('Interpolación Polinómica de Lagrange')
-ax[1].text(-4, np.min(f_original(x_values_polinomic)), error_text_polinomic, fontsize=8, va='bottom', ha='left', color='red',  bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
+ax[1].text(-4, np.min(f_original(x_values_polinomic))-2.5, error_text_polinomic, fontsize=8, va='bottom', ha='left', color='red',  bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
 ax[1].set_xlabel('x')
 ax[1].set_ylabel('y')
 ax[1].legend()
@@ -139,13 +139,13 @@ ax[2].set_ylabel('y')
 ax[2].legend()
 ax[2].grid(True)
 
-
-
-
-
 plt.subplots_adjust(bottom=0.2)
 plt.tight_layout()
 plt.show()
+
+
+###GRAF 2 CHEB Y LAGRANGE
+
 
 ##generador de distintos puntos 
 x_interp_lagrange_10 = np.linspace(-4, 4, 10)
@@ -158,24 +158,31 @@ y_interp_lagrange_15 = f_original(x_interp_lagrange_15)
 interpolator_poli_15 = lagrange(x_interp_lagrange_15, y_interp_lagrange_15)
 error_text_lagrange15 = calculate_errors(f_original(x_values_polinomic), interpolator_poli_15(x_values_polinomic))
 
+# Lagrange interpolation with 10 Chebyshev points
 x_cheb10 = chebyshev_points(a, b, 10)
 y_cheb10 = f_original(x_cheb10)
 poly10 = lagrange(x_cheb10, y_cheb10)
-
 y_interpolated_bychev10 = poly10(x_chev_tograf)
 error_text_chevbyshev10 = calculate_errors(y_original, y_interpolated_bychev10)
 
+x_cheb15 = chebyshev_points(a, b, 15)
+y_cheb15 = f_original(x_cheb15)
+poly15 = lagrange(x_cheb15, y_cheb15)
+y_interpolated_bychev15 = poly15(x_chev_tograf)
+error_text_chevbyshev15 = calculate_errors(y_original, y_interpolated_bychev15)
 
 
 
-fig2 = plt.figure(figsize=(10, 7))
+fig2 = plt.figure(figsize=(20, 10))
 
+######3falta cambiarlo para que tenga 8 puntos
 ax1 = fig2.add_subplot(3, 2, 1)
 ax1.plot(x_values_polinomic, f_original(x_values_polinomic), label='Función Original', color='orange')
 ax1.plot(x_values_polinomic, y_interp_points, label='Interpolación', linestyle='--', color='red')
 ax1.scatter(x_interp_lagrange, y_interp_lagrange, label='Puntos de Datos', color='black')
+ax1.legend(fontsize='small', bbox_to_anchor=(1.05, 1), loc='upper left')
 ax1.set_title('Lagrange con puntos equiespaciados')
-ax1.text(-4, np.min(f_original(x_values_polinomic)), error_text_polinomic, fontsize=6, va='bottom', ha='left', color='red',  bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
+ax1.text(-4, np.min(f_original(x_values_polinomic))-3, error_text_polinomic, fontsize=5, va='bottom', ha='left', color='red',  bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
 ax1.set_xlabel('x')
 ax1.set_ylabel('y')
 ax1.legend()
@@ -185,11 +192,12 @@ ax1.grid(True)
 ax2 = fig2.add_subplot(3, 2, 2)
 ax2.plot(x_chev_tograf, y_original, label='Función Original')
 ax2.plot(x_chev_tograf, y_interpolated_bychev, label='Interpolación', linestyle='--', color='red')
-ax2.plot(x_cheb, y_cheb, label='Puntos de Chebyshev', color='black')
+ax2.scatter(x_cheb, y_cheb, label='10 Puntos de Chebyshev', color='black')
+ax2.legend(fontsize='small', loc='upper left', bbox_to_anchor=(1.05, 1))
 ax2.set_xlabel('x')
 ax2.set_ylabel('y')
 ax2.set_title('Lagrange con puntos de Chebyshev')
-ax2.text(-4, np.min(y_original), error_text_chevbyshev, fontsize=6, va='bottom', ha='left', color='red', bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
+ax2.text(-4, np.min(y_original), error_text_chevbyshev, fontsize=5, va='bottom', ha='left', color='red', bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
 ax2.legend()
 ax2.grid(True)
 
@@ -198,17 +206,21 @@ ax3 = fig2.add_subplot(3, 2, 3)
 ax3.plot(x_values_polinomic, f_original(x_values_polinomic), label='Función Original', color='orange')
 ax3.plot(x_values_polinomic, interpolator_poli_10(x_values_polinomic), label='Interpolación', linestyle='--', color='red')
 ax3.scatter(x_interp_lagrange_10, y_interp_lagrange_10, label='Puntos de Datos', color='black')
+ax3.legend(fontsize='small', loc='upper left', bbox_to_anchor=(1.05, 1))
 ax3.set_title('10 puntos equiespaciados')
-ax3.text(-4, np.min(f_original(x_values_polinomic))-2.3, error_text_lagrange10, fontsize=6, va='bottom', ha='left', color='red',  bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
+ax3.text(-4, np.min(f_original(x_values_polinomic))-3, error_text_lagrange10, fontsize=6, va='bottom', ha='left', color='red',  bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
 ax3.set_xlabel('x')
 ax3.set_ylabel('y')
 ax3.legend()
 ax3.grid(True)
 
+#lagrange con 10 puntos chebvyshev
+
 ax4 = fig2.add_subplot(3, 2, 4)
 ax4.plot(x_chev_tograf, y_original, label='Función Original')
 ax4.plot(x_chev_tograf, y_interpolated_bychev10, label='Interpolación', linestyle='--', color='red')
-ax4.plot(x_cheb10, y_cheb10, label='Puntos de Chebyshev', color='black')
+ax4.scatter(x_cheb10, y_cheb10, label='Puntos de Chebyshev', color='black')
+ax4.legend(fontsize='small', loc='upper left', bbox_to_anchor=(1.05, 1))
 ax4.set_xlabel('x')
 ax4.set_ylabel('y')
 ax4.set_title('Lagrange con 10 puntos de Chebyshev')
@@ -223,19 +235,80 @@ ax5 = fig2.add_subplot(3, 2, 5)
 ax5.plot(x_values_polinomic, f_original(x_values_polinomic), label='Función Original', color='orange')
 ax5.plot(x_values_polinomic, interpolator_poli_15(x_values_polinomic), label='Interpolación', linestyle='--', color='red')
 ax5.scatter(x_interp_lagrange_15, y_interp_lagrange_15, label='Puntos de Datos', color='black')
+ax5.legend(fontsize='small', loc='upper left', bbox_to_anchor=(1.05, 1))
 ax5.set_title('15 puntos equiespaciados')
-ax5.text(-4, np.min(f_original(x_values_polinomic))-2.5, error_text_lagrange15, fontsize=6, va='bottom', ha='left', color='red',  bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
+ax5.text(-4, np.min(f_original(x_values_polinomic))-3, error_text_lagrange15, fontsize=6, va='bottom', ha='left', color='red',  bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
 ax5.set_xlabel('x')
 ax5.set_ylabel('y')
 ax5.legend()
 ax5.grid(True)
 
 
+# #lagrange con 10 puntos chebvyshev
+# Lagrange interpolation with 15 Chebyshev points
 
-
-
+ax6 = fig2.add_subplot(3, 2, 6)
+ax6.plot(x_chev_tograf, y_original, label='Función Original')
+ax6.plot(x_chev_tograf, y_interpolated_bychev15, label='Interpolación', linestyle='--', color='red')
+ax6.scatter(x_cheb15, y_cheb15, label='15 Puntos de Chebyshev', color='black')
+ax6.legend(fontsize='small', loc='upper left', bbox_to_anchor=(1.05, 1))
+ax6.set_xlabel('x')
+ax6.set_ylabel('y')
+ax6.set_title('Lagrange con 15 puntos de Chebyshev')
+ax6.text(-4, np.min(y_original), error_text_chevbyshev15, fontsize=6, va='bottom', ha='left', color='red', bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
+ax6.legend()
+ax6.grid(True)
 
 
 plt.tight_layout()
 plt.show()
 
+
+
+#GRAF ERROR VS CANTIDAD DE PUNTOS
+
+
+# Generate points for x-axis
+num_points = range(2, 14)
+
+# Initialize lists to store error values
+lagrange_errors = []
+chebyshev_errors = []
+lineal_errors = []
+spline_errors = []
+
+# Calculate errors for different number of points
+for n in num_points:
+    # Lagrange interpolation
+    x_interp_lagrange = np.linspace(-4, 4, n)
+    y_interp_lagrange = f_original(x_interp_lagrange)
+    interpolator_poli = lagrange(x_interp_lagrange, y_interp_lagrange)
+    interpolated_values = interpolator_poli(x_values_polinomic)
+    lagrange_errors.append(np.max(relative_error(f_original(x_values_polinomic), interpolated_values)))
+    
+    # Lagrange interpolation with Chebyshev points
+    x_cheb = chebyshev_points(a, b, n)
+    y_cheb = f_original(x_cheb)
+    poly = lagrange(x_cheb, y_cheb) 
+    interpolated_values = poly(x_chev_tograf)
+    chebyshev_errors.append(np.max(relative_error(y_original, interpolated_values)))
+    
+
+    
+    # Cubic spline interpolation
+    x_interp_splines = np.linspace(-4, 4, n)
+    y_interp_splines = f_original(x_interp_splines)
+    spline = CubicSpline(x_interp_splines, y_interp_splines)
+    interpolated_values = spline(x_value_splines)
+    spline_errors.append(np.max(relative_error(f_original(x_value_splines), interpolated_values)))
+
+# Plotting the errors
+plt.plot(num_points, lagrange_errors, label='Lagrange polinomico')
+plt.plot(num_points, chebyshev_errors, label='Lagrange con puntos Chebyshev')
+plt.plot(num_points, spline_errors, label='Splines Cubicos')
+plt.xlabel('Cantidad de puntos')
+plt.ylabel('Error relativo')
+plt.title('Error(relativo) vs Cantidad de puntos')
+plt.legend()
+plt.grid(True)
+plt.show()
--- 1_b.py
+++ 1_b.py
@@ -19,14 +19,14 @@ z_mesh_orig = original_function(x1_mesh_orig, x2_mesh_orig)
 
 
 #Puntos para interpolar
-x1 = np.linspace(-1, 1, 20)
-x2 = np.linspace(-1, 1, 20)
+x1 = np.linspace(-1, 1, 15)
+x2 = np.linspace(-1, 1, 15)
 X1, X2 = np.meshgrid(x1, x2)
 Z = original_function(X1, X2)
 f = RectBivariateSpline(x1, x2, Z) #interpolacion
 
-x1_interp = np.linspace(-1, 1, 20)
-x2_interp = np.linspace(-1, 1, 20)
+x1_interp = np.linspace(-1, 1, 100) ##para graf
+x2_interp = np.linspace(-1, 1, 100)
 z_interp = f(x1_interp, x2_interp)
 x1_interp_mesh, x2_interp_mesh = np.meshgrid(x1_interp, x2_interp)
 
@@ -35,89 +35,56 @@ def chebyshev_points(a, b, n):
     k = np.arange(1, n+1)
     return 0.5 * (a + b) + 0.5 * (b - a) * np.cos((2 * k - 1) / (2 * n) * np.pi)
 
-x1_cheb_interp = np.sort(chebyshev_points(-1, 1, 20))
-x2_cheb_interp = np.sort(chebyshev_points(-1, 1, 20))
-z_cheb = f(x1_cheb_interp, x2_cheb_interp)
-X1_cheb_mesh, X2_cheb_mesh = np.meshgrid(x1_cheb_interp, x2_cheb_interp)
+x1_cheb = np.sort(chebyshev_points(-1, 1, 15))
+x2_cheb = np.sort(chebyshev_points(-1, 1, 15))
+X1_cheb_mesh, X2_cheb_mesh = np.meshgrid(x1_cheb, x2_cheb)
+z_cheb = original_function(X1_cheb_mesh, X2_cheb_mesh)
+
+f_b = RectBivariateSpline(x1_cheb, x2_cheb, z_cheb)
+z_b_interp = f_b(x1_interp, x2_interp)
+
 
 
 # error rela y abs
 error_interp = np.abs(original_function(x1_interp_mesh, x2_interp_mesh) - f(x1_interp, x2_interp))/np.abs(original_function(x1_interp_mesh, x2_interp_mesh))
 error_interp_abs = np.abs(original_function(x1_interp_mesh, x2_interp_mesh) - f(x1_interp, x2_interp))
-error_cheb = np.abs(original_function(x1_cheb_interp, x2_cheb_interp) - f(x1_cheb_interp, x2_cheb_interp))/np.abs(original_function(x1_cheb_interp, x2_cheb_interp))
-error_cheb_abs = np.abs(original_function(x1_cheb_interp, x2_cheb_interp) - f(x1_cheb_interp, x2_cheb_interp))
+error_cheb = np.abs(original_function(x1_cheb, x2_cheb) - f_b(x1_cheb, x2_cheb))/np.abs(original_function(x1_cheb, x2_cheb))
+error_cheb_abs = np.abs(original_function(x1_cheb, x2_cheb) - f_b(x1_cheb, x2_cheb))
 
-error_abs_max = np.max(error_interp_abs)
-error_rel_max = np.max(error_interp)
-error_cheb_rel_max = np.max(error_cheb)
-error_cheb_abs_max = np.max(error_cheb_abs)
+error_abs_max = np.median(error_interp_abs)
+error_rel_max = np.median(error_interp)
+error_cheb_rel_max = np.median(error_cheb)
+error_cheb_abs_max = np.median(error_cheb_abs)
 
 equi_text = f"Error relativo:\n{error_rel_max}\nError absoluto:\n{error_abs_max}"
 cheb_text = f"Error relativo:\n{error_cheb_rel_max}\nError absoluto:\n{error_cheb_abs_max}"
 
-##hacer un grafico que muestre mientras mas puntos el error de cada una de las interpolaciones
-def error_puntos(funcion, x1, x2, puntos):
-    error = []
-    for i in range(2, puntos+1):
-        x1_point = np.linspace(-1, 1, i)
-        y_point = np.linspace(-1, 1, i)
-        z_point = funcion(x1_point, y_point)
-        error.append(np.max(np.abs(original_function(x1_point, y_point) - z_point))/np.max(np.abs(original_function(x1_point, y_point))))
-    return error
+
 
 
 fig = plt.figure(figsize=(12, 9))
 
-ax1 = fig.add_subplot(2, 3, 1, projection='3d')
+ax1 = fig.add_subplot(1, 3, 1, projection='3d')
 ax1.plot_surface(x1_mesh_orig, x2_mesh_orig, z_mesh_orig, cmap='viridis')
 ax1.set_title('Función Original')
 
 # función interpolada
-ax2 = fig.add_subplot(2, 3, 2, projection='3d')
+ax2 = fig.add_subplot(1, 3, 2, projection='3d')
 ax2.plot_surface(x1_interp_mesh, x2_interp_mesh, z_interp, cmap='plasma')
 ax2.text2D(0.05, -0.2, equi_text, transform=ax2.transAxes, fontsize=10, color='red', bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
 ax2.set_title('Función Interpolada')
 
 #  función chebvyshev
-ax3 = fig.add_subplot(2, 3, 3, projection='3d') 
-ax3.plot_surface(X1_cheb_mesh, X2_cheb_mesh, z_cheb, cmap='Blues')
+ax3 = fig.add_subplot(1, 3, 3, projection='3d') 
+ax3.plot_surface(x1_interp_mesh, x2_interp_mesh, z_b_interp, cmap='Blues')
 ax3.text2D(0.05,-0.2, cheb_text, transform=ax3.transAxes, fontsize=10, color='red', bbox=dict(facecolor='white', alpha=0.5, edgecolor='black'))
 ax3.set_title('Función chebvyshev')
-
-# ##hacer un grafico que muestre mientras mas puntos el error de cada una de las interpolaciones
-
-
-# Graficar el error de cada interpolación
-num_points = range(2, 21)
-error_equi = error_puntos(f, x1, x2, 20)
-error_chebyshev = error_puntos(f, x1_cheb_interp, x2_cheb_interp, 20)
-
-#corregir esto!!!!!!
-'''ax4 = fig.add_subplot(2, 3, 5)
-ax4.plot(num_points, error_equi, marker='o', label='Equidistant')
-ax4.set_xlabel('Number of Points')
-ax4.set_ylabel('Relative Error')
-ax4.set_title('Error vs Number of Points (Equidistant)')
-ax4.legend()
-
-ax5 = fig.add_subplot(2, 3, 6)
-ax5.plot(num_points, error_chebyshev, marker='o', label='Chebyshev')
-ax5.set_xlabel('Number of Points')
-ax5.set_ylabel('Relative Error')
-ax5.set_title('Error vs Number of Points (Chebyshev)')
-ax5.legend()'''
-
-
-
 
 plt.subplots_adjust(bottom=0.2)
 plt.tight_layout()
 plt.show()
 
-
-
-
-
+# hacer una funcion que muestre como cambia el error en funcion de como aumenta la cantidad de nodos
 
 
 
