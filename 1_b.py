16a3eb99c67471dc6b1121ff61462b40ebccf1be
Anaholcman <anitaholcman@gmail.com>
2024-03-27 11:36:27 -0300

    1a y b corregido

--- 1_b.py
+++ 1_b.py
@@ -44,16 +44,28 @@ f_b = RectBivariateSpline(x1_cheb, x2_cheb, z_cheb)
 z_b_interp = f_b(x1_interp, x2_interp)
 
 
+def absolute_error(func, x1_evaluar, x2_evaluar):
+    return np.abs(original_function(x1_evaluar, x2_evaluar)-func(x1_evaluar, x2_evaluar))
 
-# error rela y abs
+def relative_error(func, x1_evaluar, x2_evaluar):
+    return np.abs(original_function(x1_evaluar, x2_evaluar)-func(x1_evaluar, x2_evaluar))/np.abs(original_function(x1_evaluar, x2_evaluar))
+
+
+error_abs = absolute_error(f, x1_interp, x2_interp)
+error_rel = relative_error(f, x1_interp, x2_interp)
+error_cheb_abs = absolute_error(f_b, x1_interp, x2_interp)
+error_cheb_rel = relative_error(f_b, x1_interp, x2_interp)
+
+
+'''
 error_interp = np.abs(original_function(x1_interp_mesh, x2_interp_mesh) - f(x1_interp, x2_interp))/np.abs(original_function(x1_interp_mesh, x2_interp_mesh))
 error_interp_abs = np.abs(original_function(x1_interp_mesh, x2_interp_mesh) - f(x1_interp, x2_interp))
 error_cheb = np.abs(original_function(x1_cheb, x2_cheb) - f_b(x1_cheb, x2_cheb))/np.abs(original_function(x1_cheb, x2_cheb))
-error_cheb_abs = np.abs(original_function(x1_cheb, x2_cheb) - f_b(x1_cheb, x2_cheb))
+error_cheb_abs = np.abs(original_function(x1_cheb, x2_cheb) - f_b(x1_cheb, x2_cheb))'''
 
-error_abs_max = np.median(error_interp_abs)
-error_rel_max = np.median(error_interp)
-error_cheb_rel_max = np.median(error_cheb)
+error_abs_max = np.median(error_abs)
+error_rel_max = np.median(error_rel)
+error_cheb_rel_max = np.median(error_cheb_rel)
 error_cheb_abs_max = np.median(error_cheb_abs)
 
 equi_text = f"Error relativo:\n{error_rel_max}\nError absoluto:\n{error_abs_max}"
@@ -85,8 +97,52 @@ plt.tight_layout()
 plt.show()
 
 # hacer una funcion que muestre como cambia el error en funcion de como aumenta la cantidad de nodos
+def plot_error_vs_nodes():
+    num_nodes = np.arange(5, 50, 5)
+    error_abs_max = []
+    error_rel_max = []
+    error_cheb_rel_max = []
+    error_cheb_abs_max = []
 
+    for n in num_nodes:
+        x1 = np.linspace(-1, 1, n)
+        x2 = np.linspace(-1, 1, n)
+        X1, X2 = np.meshgrid(x1, x2)
+        Z = original_function(X1, X2)
+        f = RectBivariateSpline(x1, x2, Z)
 
+        x1_interp = np.linspace(-1, 1, 100)
+        x2_interp = np.linspace(-1, 1, 100)
+        z_interp = f(x1_interp, x2_interp)
 
+        error_abs = absolute_error(f, x1_interp, x2_interp)
+        error_rel = relative_error(f, x1_interp, x2_interp)
 
+        error_abs_max.append(np.median(error_abs))
+        error_rel_max.append(np.median(error_rel))
 
+        x1_cheb = np.sort(chebyshev_points(-1, 1, n))
+        x2_cheb = np.sort(chebyshev_points(-1, 1, n))
+        X1_cheb_mesh, X2_cheb_mesh = np.meshgrid(x1_cheb, x2_cheb)
+        z_cheb = original_function(X1_cheb_mesh, X2_cheb_mesh)
+
+        f_b = RectBivariateSpline(x1_cheb, x2_cheb, z_cheb)
+        z_b_interp = f_b(x1_interp, x2_interp)
+
+        error_cheb_abs = absolute_error(f_b, x1_interp, x2_interp)
+        error_cheb_rel = relative_error(f_b, x1_interp, x2_interp)
+
+        error_cheb_abs_max.append(np.median(error_cheb_abs))
+        error_cheb_rel_max.append(np.median(error_cheb_rel))
+
+    plt.plot(num_nodes, error_abs_max, label='Absolute Error')
+    plt.plot(num_nodes, error_rel_max, label='Relative Error')
+    plt.plot(num_nodes, error_cheb_abs_max, label='Chebyshev Absolute Error')
+    plt.plot(num_nodes, error_cheb_rel_max, label='Chebyshev Relative Error')
+    plt.xlabel('Number of Nodes')
+    plt.ylabel('Error')
+    plt.title('Error vs Number of Nodes')
+    plt.legend()
+    plt.show()
+
+plot_error_vs_nodes()
